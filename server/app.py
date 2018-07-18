a3b237673fe6fb7f4be3f0dcd64f71991cd8daa8
Pratik Nagelia <pratik.nagelia@flipkart.com>
2018-07-18 12:53:47 +0530

    Added flask server

---
+++ server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from controller import *
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Welcome to Patrick's Den !!"
+
+@app.route("/<gpio>/<state>")
+def switch(gpio, state):
+    return gpio + state
+
+
+if __name__ == "__main__":
+    init()
+    app.run()
---
+++ server/controller.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import sys
+
+
+# GPIO | Relay
+# --------------
+# 26     01
+# 19     02
+# 13     03
+# 06     04
+# 12     05
+# 16     06
+# 20     07
+# 21     08
+
+# initiate list with pin gpio pin numbers
+
+def switch(gpio, state):
+    GPIO.setup(gpio, GPIO.OUT)
+    if state == "ON":
+        GPIO.output(gpio, GPIO.LOW)
+    elif state == "OFF":
+        GPIO.output(gpio, GPIO.HIGH)
+
+
+def init():
+    GPIO.setmode(GPIO.BCM)
